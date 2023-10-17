1951cdc29e41f7a23dce6b4f88b0e51e05a9f147
GeG712 <e249795@sbl.ch>
2023-10-17 11:23:20 +0200

    start

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function sayHello() {
-  alert("Hello World!")
+  alert("Say Hello!")
 }
 
 function changeBackground() {
   const body = document.querySelector("body")
   body.style.backgroundColor = "darkgrey"
+}
+
+function deleteText() {
+  const elemToDeleteText = document.querySelector("#to-delete")
+  elemToDeleteText.textContent = "Inhalt gelöscht"
+}
+
+
+assert.equal(typeof incrementCounter, "function") {
+  
 }
