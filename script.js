ae11b7c92eef365ee61413501846e77112c01fad
GeG712 <e249795@sbl.ch>
2023-10-20 08:33:42 +0200

    5/6 combo

--- script.js
+++ script.js
@@ -19,3 +19,16 @@ function incrementCounter() {
   numberElement.textContent = num
 }
 
+function decrementCounter() {
+  const numberElement = document.querySelector("#counter")
+  let num = parseInt(numberElement.textContent)
+  num = num - 1
+  numberElement.textContent = num
+}
+
+function resetCounter() {
+  const numberElement = document.querySelector("#counter")
+  let num = parseInt(numberElement.textContent)
+  num = 0
+  numberElement.textContent = num
+}
